d090291ec8ce484c3a9dd35fc3e7a8231dea557b
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2021-07-08 11:26:34 +0200

    Update global.R

--- inst/monobinShiny/global.R
+++ inst/monobinShiny/global.R
@@ -2,3 +2,4 @@ monobin.algo <- c("cum.bin", "iso.bin", "ndr.bin", "pct.bin", "sts.bin", "woe.bi
 rv <- reactiveValues(sync = 0, sync2 = 0, sync3 = 0, sync23 = 0, dm.uptd = 0, import.dummy = FALSE, 
 		    dwnl.sync = 0, db = NULL, target.select.2 = NULL, target.select.3 = NULL, 
 		    num.rf = NULL, rf.imp = NULL, rf.out = NULL)
+
