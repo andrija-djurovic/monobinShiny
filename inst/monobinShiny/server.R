1b5f9c1a1c42f82d9fd9e7bdcc17e6f60e215e6c
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2021-07-08 11:21:42 +0200

    Add files via upload

---
+++ inst/monobinShiny/server.R
@@ -0,0 +1,60 @@
+server <- function(input, output, session) {
+ 	session$onSessionEnded(function() {stopApp()})
+
+	algo.ui(id = "monobin")
+	dm.server(id = "data.manager")
+	di.server(id = "desc.imputation")
+	mb.server(id = "monobin")
+
+	observeEvent(rv$dm.uptd, {
+		upd.dm(id = "data.manager", dummy = rv$import.dummy)
+		}, ignoreInit = TRUE)
+
+	observeEvent(rv$sync, {
+		sync.m23(id = "desc.imputation", 
+			  num.rf = rv$num.rf,
+			  module = "desc")
+		sync.m23(id = "monobin", 
+			  num.rf = rv$num.rf,
+			  module = "monobin")
+		rv$rf.imp <- NULL
+		rv$rf.out <- NULL
+		}, ignoreInit = TRUE)
+
+	observeEvent(rv$sync2, {
+		rf.update.2 <- c(rv$num.rf[!rv$num.rf%in%rv$target.select.2], 
+				     rv$rf.imp, rv$rf.out)
+		sync.m23.imp(id = "desc.imputation", 
+				 num.rf = rf.update.2,
+				 module = "desc")
+		}, ignoreInit = TRUE)
+
+	observeEvent(rv$sync3, {
+		rf.update.3 <- c(rv$num.rf[!rv$num.rf%in%rv$target.select.3], 
+				     rv$rf.imp, rv$rf.out)
+		sync.m23.imp(id = "monobin", 
+				 num.rf = rf.update.3,
+				 module = "monobin")
+		}, ignoreInit = TRUE)
+
+	observeEvent(rv$sync23, {
+		rf.update.2 <- c(rv$num.rf[!rv$num.rf%in%rv$target.select.2], 
+				     rv$rf.imp, rv$rf.out)
+		rf.update.3 <- c(rv$num.rf[!rv$num.rf%in%rv$target.select.3], 
+				     rv$rf.imp, rv$rf.out)
+		sync.m23.imp(id = "desc.imputation", 
+				 num.rf = rf.update.2,
+				 module = "desc")
+		sync.m23.imp(id = "monobin", 
+				 num.rf = rf.update.3,
+				 module = "monobin")
+		}, ignoreInit = TRUE)
+
+	observeEvent(rv$dwnl.sync, {
+		hide.dwnl.buttons(id = "desc.imputation")
+		}, ignoreInit = TRUE)
+
+}
+
+
+
---
+++ inst/monobinShiny/ui.R
@@ -0,0 +1,16 @@
+ui <- dashboardPage(skin = "black", title = "Shiny interface for monobin package", 
+
+dashboardHeader(title = HTML("Shiny interface for 
+				     <a href = 'https://cran.r-project.org/web/packages/monobin/monobin.pdf'>
+				     monobin</a> package"),
+		    titleWidth = "500px"),
+
+dashboardSidebar(disable = TRUE),
+
+dashboardBody(
+
+	dm.ui(id = "data.manager"),
+	di.ui(id = "desc.imputation"),
+	mb.ui(id = "monobin")
+)
+)
